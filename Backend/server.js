9bab8cb2e8ba7909e54aecd3cf05c6a7afe902b8
Mayowa <adesinae940@gmail.com>
2026-09-11 17:03:50 +0100

    Fix CORS for frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const fs = require("fs");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = "users.json";
 
